3dccae655ee5a2fa2245522675e5039f60ba640f
Surya Lolla <slolla@paypal.com>
2023-10-14 16:12:19 +0530

    git commit

--- product.py
+++ product.py
@@ -1,4 +1,5 @@
-x=int(input('Enter first digit='))
-y=int(input('Enter 2nd integer='))
-product=x*y
-print(product)
+x = int(input("Enter the value of mul table= "))
+for y in range (1 , 11):
+   # print(x)
+    #x=int(input("Enter the value= "))
+ print(x,'x', y ,'=', x*y)
