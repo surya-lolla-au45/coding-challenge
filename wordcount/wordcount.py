d34ca181ea76183896dd47abf6959255b8a5dd1f
SURYA SAI VAMSI KRISHNA LOLLA <101198767+surya-lolla-au45@users.noreply.github.com>
2023-11-01 14:15:20 +0530

    Create wordcount.py

---
+++ wordcount/wordcount.py
@@ -0,0 +1,18 @@
+n="Data preprocessing is an important task in text classification. With the emergence of Python in the field of data science"
+words=n.split()
+word_count = {}
+for i in words:
+    i = i.strip('.,!?").').lower()
+    if i in word_count:
+        word_count[i] += 1
+    else:
+        word_count[i] = 1
+print("Unique words and their counts of occurrence:")
+for word, count in word_count.items():
+      print(f"{word}: {count}")
+ 
+
+
+
+
+
