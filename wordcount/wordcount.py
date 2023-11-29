4a2b8cb9ab430f19e517d817c8f20799ba1ae83f
SURYA SAI VAMSI KRISHNA LOLLA <101198767+surya-lolla-au45@users.noreply.github.com>
2023-11-29 16:40:38 +0530

    Update wordcount.py

--- wordcount/wordcount.py
+++ wordcount/wordcount.py
@@ -1,17 +1,39 @@
-n="Data preprocessing is an important task in text classification. With the emergence of Python in the field of data science"
-words=n.split()
-word_count = {}
-for i in words:
-    if i in word_count:
-        word_count[i] += 1
-    else:
-        word_count[i] = 1
-print("Unique words and their counts of occurrence:")
-for word, count in word_count.items():
-      print(f"{word}: {count}")
+import csv
+import re
+import json
+from collections import Counter
+
+def word_count(paragraph, output_format='csv', sort_by='word', order='asc', limit=None, filter_by=None):
+    words = re.findall(r'\b\w+\b', paragraph.lower())
+    
+    if filter_by:
+        words = [word for word in words if re.search(filter_by, word)]
+
+    word_count = Counter(words)
+
+    if sort_by == 'word':
+        word_count = dict(sorted(word_count.items()))
+    elif sort_by == 'occurrence':
+        word_count = dict(sorted(word_count.items(), key=lambda item: item[1]))
+
+
+    if order == 'desc':
+        word_counts = dict(reversed(list(word_count.items())))
  
+    if limit:
+        word_counts = dict(list(word_count.items())[:limit])
 
+    if output_format == 'csv':
+        with open('word_count.csv', 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            csv_writer.writerow(['Word', 'Occurrence'])
+            csv_writer.writerows(word_count.items())
+        print('Word count saved to "word_count.csv" in CSV format.')
+    elif output_format == 'json':
+        with open('word_count.json', 'w') as json_file:
+            json.dump(word_counts, json_file, indent=2)
+        print('Word count saved to "word_count.json" in JSON format.')
+ 
+paragraph = "This is a sample paragraph. It contains sample words for word count. The words can be counted based on specified options."
 
-
-
-
+word_count(paragraph, output_format='csv', sort_by='occurrence', order='desc', limit=5, filter_by='s*')
